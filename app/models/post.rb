b5ea61b8b06b1dba1b9f800966e645b699125b9b
Avi Arenfeld <aviarenfeld@gmail.com>
2014-12-14 11:18:15 -0600

    Add pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
